e22693a3c42733b23047960dc0ea1c503f104418
“shmenkare” <shmenkare@gmail.com>
2023-10-05 01:14:21 -0400

    V2.0 agregadas funciones

--- Program.cs
+++ Program.cs
@@ -9,63 +9,91 @@ namespace ProdVehiculos
     {
         static void Main(string[] args)
         {
+            //V2.0 con funciones agragadas.
+            
             int i = 0, j = 0, ventaDia = 0, totDias = 20,cantMay = 0, cantMen = 100;
             float sum = 1.0f, prom = 1.0f;
             
             int [,] VentaSemanal = new int [4,5];
 
+
             for (i = 0; i < 4; i++)
             {
                 for (j = 0; j < 5; j++)
                 {
-                    Console.WriteLine("Cantidad de vehiculos producidos en el dia {0}{1}",i,j);
+                    Console.WriteLine("Cantidad de vehiculos producidos en el dia {0}{1}", i, j);
                     ventaDia = Convert.ToInt32(Console.ReadLine());
-                    VentaSemanal[i,j] = ventaDia;
+                    VentaSemanal[i, j] = ventaDia;
                     sum += ventaDia;
                     Console.Clear();
                 }
             }
+            MostrarTabla(VentaSemanal);
+            
+            prom = Promedio(sum, totDias);
+            Console.WriteLine("El promedio de ventas es {0} ", prom);
+
+            cantMen = Menor(VentaSemanal, cantMen);
+            cantMay = Mayor(VentaSemanal, cantMay);
+
+            Console.WriteLine("La menor cantidad producida es {0} ", cantMen);
+            Console.WriteLine("La mayor cantidad producida es {0} ", cantMay);
+            
+            Console.ReadKey();
+            
+        }
+        static void MostrarTabla(int [,] Array)
+        {
+            int i = 0, j = 0;
             Console.WriteLine("Mostrando tabla");
             for (i = 0; i < 4; i++)
             {
                 for (j = 0; j < 5; j++)
                 {
                     Thread.Sleep(100);
-                    Console.Write(VentaSemanal[i, j]);
+                    Console.Write(Array[i, j]+" ");
                     
                 }
                 Console.WriteLine();
             }
-            prom = sum / totDias;
-
-            Console.WriteLine("El promedio de ventas es {0} ", prom);
+        }
+        static int Menor(int [,]Array, int valor)
+        {
+            int i = 0, j = 0;
             for (i = 0; i < 4; i++)
             {
                 for (j = 0; j < 5; j++)
                 {
-                    if (VentaSemanal[i, j] < cantMen)
+                    if (Array[i, j] < valor)
                     {
-                        cantMen = VentaSemanal[i,j];
+                        valor = Array[i,j];
                     }
                 }
                 
             }
+            return valor;
+        }
+        static int Mayor(int [,] Array, int valor) 
+        {
+            int i = 0, j = 0;
             for (i = 0; i < 4; i++)
             {
                 for (j = 0; j < 5; j++)
                 {
-                    if (VentaSemanal[i, j] > cantMay)
+                    if (Array[i, j] > valor)
                     {
-                        cantMay = VentaSemanal[i, j];
+                        valor = Array[i, j];
                     }
                 }
 
             }
-            Console.WriteLine("La menor cantidad producida es {0} ", cantMen);
-            Console.WriteLine("La mayor cantidad producida es {0} ", cantMay);
-            Console.ReadKey();
-            
-
+            return valor;
         }
+        static float Promedio(float a, float b) 
+        {
+            float res = a / b;
+            return res;
+        }
+        
     }
 }
